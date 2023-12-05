9263159c0c2cf4d7d6705c0fbef5cbe4a6d51a4f
Marina Yanysheva <yme@incom-sr.ru>
2023-12-05 13:39:57 +0300

    add win/lose logic

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,12 @@
 		<GameWord :word="word" :correct-letters="correctLetters" />
 	</div>
 
-	<GamePopup v-if="false" />
+	<GamePopup ref="popup" :word="word" @restart="restart" />
 	<GameNotification ref="notification" />
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 import GameHeader from "./components/GameHeader.vue";
 import GameFugure from "./components/GameFugure.vue";
 import GameWrongLetters from "./components/GameWrongLetters.vue";
@@ -21,16 +21,36 @@ import GameNotification from "./components/GameNotification.vue";
 
 const word = ref("василий");
 const letters = ref<string[]>([]);
-const notification = ref<InstanceType<typeof GameNotification> | null>(null);
 
+const notification = ref<InstanceType<typeof GameNotification> | null>(null);
+const popup = ref<InstanceType<typeof GamePopup> | null>(null);
 const correctLetters = computed(() =>
 	letters.value.filter((el) => word.value.includes(el))
 );
 const wrongLetters = computed(() =>
 	letters.value.filter((el) => !word.value.includes(el))
 );
+const isLose = computed(() => wrongLetters.value.length === 6);
+const isWin = computed(() =>
+	[...word.value].every((el) => correctLetters.value.includes(el))
+);
+
+watch(wrongLetters, () => {
+	if (isLose.value) {
+		popup.value?.openPopup("lose");
+	}
+});
+
+watch(wrongLetters, () => {
+	if (isWin.value) {
+		popup.value?.openPopup("win");
+	}
+});
 
 window.addEventListener("keydown", ({ key }) => {
+	if (isLose.value || isWin.value) {
+		return;
+	}
 	if (letters.value.includes(key)) {
 		notification.value?.showNotification();
 		setTimeout(() => notification.value?.hideNotification(), 2000);
@@ -40,6 +60,11 @@ window.addEventListener("keydown", ({ key }) => {
 		letters.value.push(key.toLowerCase());
 	}
 });
+
+const restart = () => {
+	letters.value = [];
+	popup.value?.closePopup();
+};
 </script>
 
 <style scoped></style>
--- src/components/GamePopup.vue
+++ src/components/GamePopup.vue
@@ -1,14 +1,50 @@
 <template>
-	<div class="popup-container">
+	<div v-if="isVisible" class="popup-container">
 		<div class="popup">
-			<h2>Поздравляю, вы победили! 😃</h2>
-			<!-- <h2>Вы проиграли. 😕</h2> -->
-			<!-- <h3>...имя: Лидия</h3> -->
-			<button>Сыграть еще раз</button>
+			<h2 v-if="gameStatus === 'win'">Поздравляю, вы победили! 😃</h2>
+			<template v-else>
+				<h2>Вы проиграли. 😕</h2>
+				<h3>...имя: {{ word }}</h3>
+			</template>
+			<button @click="emit('restart')">Сыграть еще раз</button>
+			<!-- <button @click="emit('restart', 3)">Сыграть еще раз</button> -->
 		</div>
 	</div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import type { GameStatus } from "../types/GameStatus";
+
+interface Props {
+	word: string;
+}
+
+defineProps<Props>();
+
+const gameStatus = ref<GameStatus | null>(null);
+const isVisible = ref(false);
+
+const openPopup = (status: GameStatus) => {
+	gameStatus.value = status;
+	isVisible.value = true;
+};
+const closePopup = () => {
+	isVisible.value = false;
+};
+
+defineExpose({
+	openPopup,
+	closePopup,
+});
+
+const emit = defineEmits<{
+	(e: "restart"): void;
+}>();
+
+// const emit = defineEmits<{
+// 	(e: "restart", id:number): void;
+// }>();
+</script>
 
 <style scoped></style>
